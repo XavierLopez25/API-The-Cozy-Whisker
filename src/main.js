e8c9603ff7febe79974d49b90c3c0de69ea05d73
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-08 16:18:38 -0600

    feat: added the use of functions for login and register from db.js and implementation of middlewares

--- src/main.js
+++ src/main.js
@@ -1,17 +1,54 @@
 import express from 'express';
+import pool from './conn.js';
+
+import { getAllTables, loginUser, registerNewEmployee } from './db.js';
 import bodyParser from 'body-parser';
 
-import { getAllTables } from './db.js';
-
+// Inicializar la aplicación Express
 const app = express();
+// Habilitar el middleware para parsear JSON en el cuerpo de las solicitudes
 app.use(express.json());
 app.use(bodyParser.json());
+
 const port = 5000;
 
 app.get('/', async (req, res) => {
   const posts = await getAllTables();
   console.log(posts);
   res.send('Hello World from API!');
+});
+
+// User Registration Endpoint
+app.post('/register', async (req, res) => {
+  const { name, role, startDate, username, password } = req.body;
+  try {
+    const result = await registerNewEmployee(name, role, startDate, username, password);
+    res.json({
+      status: 'success',
+      employeeId: result.rows[0].register_new_employee,
+    });
+  } catch (error) {
+    console.error('Error executing register_new_employee function', error.stack);
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
+// User Login Endpoint
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  console.log('TEST', username);
+  console.log('TEST', password);
+  try {
+    const result = await loginUser(username, password);
+    if (result.rows.length > 0) {
+      res.json({ status: 'success', user: result.rows[0] });
+    } else {
+      res.status(401).json({ status: 'fail', message: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.error('Error executing user_login function', error.stack);
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
