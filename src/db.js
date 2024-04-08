83a8400b9421a1d796d0b90f7176562fb492c147
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-08 16:17:29 -0600

    feat: added the query functions with pool for login and register

--- src/db.js
+++ src/db.js
@@ -2,8 +2,24 @@ import conn from './conn.js';
 
 //Get all tables
 export async function getAllTables() {
-  const result = await conn.query(
-    `SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`,
-  );
+  const result = await conn.query(`SELECT * FROM Usuario;`);
   return result.rows;
 }
+
+export async function loginUser(username, password) {
+  const result = await conn.query(`SELECT usuario, pwd_md5 FROM user_login($1::TEXT, $2::TEXT)`, [
+    username,
+    password,
+  ]);
+  return result;
+}
+
+export async function registerNewEmployee({ name, role, startDate, username, password }) {
+  const result = await conn.query(
+    `SELECT * FROM register_new_employee($1::TEXT, $2::TEXT, $3::DATE, $4::TEXT, $5::TEXT)`,
+    [name, role, startDate, username, password],
+  );
+  return result;
+}
+
+//SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
