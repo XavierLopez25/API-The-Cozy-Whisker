2e83839ef93293fe9c25875e205b4247849ace61
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-11 12:56:44 -0600

    feat: added the async function for closeCuenta

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import conn from './conn.js';
 
 //Get all tables
 export async function getAllTables() {
-  const result = await conn.query(`SELECT * FROM Usuario;`);
+  const result = await conn.query(`SELECT * FROM Cuenta;`);
   return result.rows;
 }
 
@@ -24,6 +24,13 @@ export async function registerNewEmployee(name, role, startDate, username, passw
 
 export async function insertNewCuenta(mesaId, personas) {
   await conn.query(`CALL insert_new_cuenta($1::INT, $2::INT);`, [mesaId, personas]);
+}
+
+export async function closeCuenta(mesaId, nit, dir, nombre, efectivo, tarjeta) {
+  await conn.query(
+    `CALL close_cuenta($1::INT, $2::TEXT, $3::TEXT, $4::TEXT, $5::BOOLEAN, $6::BOOLEAN);`,
+    [mesaId, nit, dir, nombre, efectivo, tarjeta],
+  );
 }
 
 //SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
