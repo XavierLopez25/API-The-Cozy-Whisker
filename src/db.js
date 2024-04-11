d94a828b1800cb9a12f0098fe5d03f4ec78ae8fe
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-11 12:27:30 -0600

    feat: added the async function for insertNewCuenta

--- src/db.js
+++ src/db.js
@@ -7,19 +7,23 @@ export async function getAllTables() {
 }
 
 export async function loginUser(username, password) {
-  const result = await conn.query(`SELECT usuario, pwd_md5 FROM user_login($1::TEXT, $2::TEXT)`, [
+  const result = await conn.query(`SELECT usuario, pwd_md5 FROM user_login($1::TEXT, $2::TEXT);`, [
     username,
     password,
   ]);
   return result;
 }
 
-export async function registerNewEmployee({ name, role, startDate, username, password }) {
+export async function registerNewEmployee(name, role, startDate, username, password) {
   const result = await conn.query(
-    `SELECT * FROM register_new_employee($1::TEXT, $2::TEXT, $3::DATE, $4::TEXT, $5::TEXT)`,
+    `SELECT * FROM register_new_employee($1::TEXT, $2::TEXT, $3::DATE, $4::TEXT, $5::TEXT);`,
     [name, role, startDate, username, password],
   );
   return result;
+}
+
+export async function insertNewCuenta(mesaId, personas) {
+  await conn.query(`CALL insert_new_cuenta($1::INT, $2::INT);`, [mesaId, personas]);
 }
 
 //SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
