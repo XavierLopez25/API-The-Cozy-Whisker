442a35b5d39096235bf7d05093ddf2480d28bba5
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-12 10:15:35 -0600

    feat: added the async function for getAvailableMesas

--- src/db.js
+++ src/db.js
@@ -94,4 +94,11 @@ export async function fetchIndividualPayments(mesa_id_arg) {
   return result.rows;
 }
 
+export async function getAvailableMesas() {
+  const result = await conn.query(
+    `SELECT mesa_id, esmovil, Area.nombre, capacidadmesa AS nombre_area FROM Mesa INNER JOIN Area ON Mesa.area_id = Area.area_id WHERE NOT EXISTS (SELECT 1 FROM Cuenta WHERE Cuenta.mesa_id = Mesa.mesa_id AND Cuenta.estado = 'Abierta');`,
+  );
+  return result.rows;
+}
+
 //SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
